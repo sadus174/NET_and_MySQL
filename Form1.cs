22a15f6e44b34cc754f764d783bcef812bf2e9d3
sadus174 <34853375+sadus174@users.noreply.github.com>
2021-10-21 20:33:34 +0500

    Добавьте файлы проекта.

---
+++ Form1.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using MySql.Data.MySqlClient;
+
+namespace NET_and_MySQL
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+        MySqlConnection conn;
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            // строка подключения к БД
+            string connStr = "server=caseum.ru;port=33333;user=test_user;database=db_test;password=test_pass;";
+            // создаём объект для подключения к БД
+            conn = new MySqlConnection(connStr);
+
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            //Объявлем переменную для запроса в БД
+            string selected_id_stud = textBox1.Text;
+            // устанавливаем соединение с БД
+            conn.Open();
+            // запрос
+            string sql = $"SELECT fio FROM t_stud WHERE id={selected_id_stud}";
+            // объект для выполнения SQL-запроса
+            MySqlCommand command = new MySqlCommand(sql, conn);
+            // выполняем запрос и получаем ответ
+            string name = command.ExecuteScalar().ToString();
+            // выводим ответ в TextBox
+            textBox2.Text = name;
+            // закрываем соединение с БД
+            conn.Close();
+        }
+    }
+}
