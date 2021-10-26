a7b70a62813c289e0831fc4e323181f0dd93f3d2
spam174 <spam174@mail.ru>
2021-10-26 08:37:55 +0500

    Добавлены коментарии на форме ComboBox

--- Form9.cs
+++ Form9.cs
@@ -21,35 +21,35 @@ namespace NET_and_MySQL
         public void GetComboBoxList()
         {
             //Формирование списка статусов
-            DataTable list_dolg_table = new DataTable();
-            MySqlCommand list_dolg_command = new MySqlCommand();
+            DataTable list_stud_table = new DataTable();
+            MySqlCommand list_stud_command = new MySqlCommand();
             //Открываем соединение
             conn_db.Open();
             //Формируем столбцы для комбобокса списка ЦП
-            list_dolg_table.Columns.Add(new DataColumn("id", System.Type.GetType("System.Int32")));
-            list_dolg_table.Columns.Add(new DataColumn("fio", System.Type.GetType("System.String")));
+            list_stud_table.Columns.Add(new DataColumn("id", System.Type.GetType("System.Int32")));
+            list_stud_table.Columns.Add(new DataColumn("fio", System.Type.GetType("System.String")));
             //Настройка видимости полей комбобокса
-            comboBox1.DataSource = list_dolg_table;
+            comboBox1.DataSource = list_stud_table;
             comboBox1.DisplayMember = "fio";
             comboBox1.ValueMember = "id";
             //Формируем строку запроса на отображение списка статусов прав пользователя
             string sql_list_users = "SELECT id, fio FROM t_stud";
-            list_dolg_command.CommandText = sql_list_users;
-            list_dolg_command.Connection = conn_db;
+            list_stud_command.CommandText = sql_list_users;
+            list_stud_command.Connection = conn_db;
             //Формирование списка ЦП для combobox'a
-            MySqlDataReader reader_list_otdel;
+            MySqlDataReader list_stud_reader;
             try
             {
-                reader_list_otdel = list_dolg_command.ExecuteReader();
-
-                while (reader_list_otdel.Read())
+                //Инициализируем ридер
+                list_stud_reader = list_stud_command.ExecuteReader();
+                while (list_stud_reader.Read())
                 {
-                    DataRow rowToAdd = list_dolg_table.NewRow();
-                    rowToAdd["id"] = Convert.ToInt32(reader_list_otdel[0]);
-                    rowToAdd["fio"] = reader_list_otdel[1].ToString();
-                    list_dolg_table.Rows.Add(rowToAdd);
+                    DataRow rowToAdd = list_stud_table.NewRow();
+                    rowToAdd["id"] = Convert.ToInt32(list_stud_reader[0]);
+                    rowToAdd["fio"] = list_stud_reader[1].ToString();
+                    list_stud_table.Rows.Add(rowToAdd);
                 }
-                reader_list_otdel.Close();
+                list_stud_reader.Close();
                 conn_db.Close();
             }
             catch (Exception ex)
@@ -70,8 +70,8 @@ namespace NET_and_MySQL
 
         private void Form9_Load(object sender, EventArgs e)
         {
+            //Инициализируем соединение
             conn_db = new MySqlConnection(connStr);
-
             //Метод заполнения ComboBox
             GetComboBoxList();
         }
@@ -87,6 +87,7 @@ namespace NET_and_MySQL
             //Читаем ридер
             while (reader_list.Read())
             {
+                //Формируем строку для вывода красивого сообщения в MessageBox
                 string s = "";
                 s += "Код студента: " + reader_list[0].ToString()+ "\n";
                 s += "ФИО: " + reader_list[1].ToString()+ "\n";
@@ -94,13 +95,16 @@ namespace NET_and_MySQL
                 s += "Возраст: " + reader_list[3].ToString() + " лет";
                 MessageBox.Show(s);
             }
+            //Закрываем ридер
             reader_list.Close();
+            //Закрываем соединение
             conn_db.Close();
         }
         private void button1_Click(object sender, EventArgs e)
         {
-
+            //Получаем из ComboBox звыбранное значение, которое является ID студента
             int id_selected_user = Convert.ToInt32(comboBox1.SelectedValue);
+            //Посылаем ID студента в метод вывода информациии
             GetList(id_selected_user);
         }
     }
